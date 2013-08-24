749faaec4969cf84b4ad0fdb2befa508963334e9
Crowbeak <crowbeak@gmail.com>
2013-08-24 19:48:54 +0900

    I think everything I need exists.

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ enchant();
 var Constants = {
     debug: true,
     blue: "#00CCFF",
+    playerSpeed: 8,
     red: "#FF0000",
     seconds: 10,
     stageHeight: 480,
@@ -82,15 +83,110 @@ var Switch = {
                 this.color = "white";
             }
         }
+    }),
+    
+    polystate: Class.create(Label, {
+        initialize: function (megapanel) {
+            "use strict";
+            Label.call(this, "FRIMS DOWN");
+            
+            this.megapanel = megapanel;
+            this.onColor = Constants.blue;
+            this.onTextColor = "black";
+            this.offColor = Constants.red;
+            this.offTextColor = "white";
+            this.height = 36;
+            this.width = 120;
+            this.x = this.megapanel.x + 20;
+            this.y = this.megapanel.y + 100;
+            
+            this.text = "OFF";
+            this.backgroundColor = this.offColor;
+            this.color = this.offTextColor;
+            this.font = "18px arial,sans-serif";
+        },
+        
+        onenterframe: function update() {
+            "use strict";
+            if (this.megapanel.state === 0) {
+                this.backgroundColor = this.offColor;
+                this.text = "OFF";
+                this.color = this.offTextColor;
+            } else if (this.megapanel.state === 1) {
+                this.backgroundColor = this.onColor;
+                this.text = "FRIMS<br>DECREASING";
+                this.color = this.onTextColor;
+            } else if (this.megapanel.state === 2) {
+                this.backgroundColor = this.onColor;
+                this.text = "PAZZLES<br>DECREASING";
+                this.color = this.onTextColor;
+            } else if (this.megapanel.state === 3) {
+                this.backgroundColor = this.onColor;
+                this.text = "GONKS<br>DECREASING";
+                this.color = this.onTextColor;
+            }
+        }
+    })
+};
+
+
+var Warning = {
+    danger: Class.create(Sprite, {
+        initialize: function (img, dial) {
+            "use strict";
+            Sprite.call(this, img.width, img.height);
+            
+            this.dial = dial;
+            this.image = img;
+            this.x = this.dial.x + (this.dial.width / 2) - (this.width / 2);
+            this.y = this.dial.y - this.height - 16;
+            this.visible = false;
+        },
+        
+        onenterframe: function updateWarning() {
+            "use strict";
+            if ((this.dial.value < this.dial.minSafe) || (this.dial.value > this.dial.maxSafe)) {
+                this.visible = true;
+            }
+        }
+    }),
+    
+    safe: Class.create(Sprite, {
+        initialize: function (img, dial) {
+            "use strict";
+            Sprite.call(this, img.width, img.height);
+            
+            this.dial = dial;
+            this.image = img;
+            this.x = this.dial.x + (this.dial.width / 2) - (this.width / 2);
+            this.y = this.dial.y - this.height - 16;
+        }
     })
 };
 
 
 var Indicator = {
+    dial: Class.create(Sprite, {
+        initialize: function (images, xCoord, minSafe, maxSafe) {
+            "use strict";
+            Sprite.call(this, images.dial.width, images.dial.height);
+            
+            this.image = images.dial;
+            this.x = xCoord;
+            this.y = 240 - this.height - 30;
+            
+            this.minSafe = minSafe;
+            this.maxSafe = maxSafe;
+            this.value = ((this.maxSafe - this.minSafe) / 2) + this.minSafe;
+            this.danger = new Warning.danger(images.warning, this);
+            this.safe = new Warning.safe(images.safe, this);
+        }
+    }),
+    
     panel: Class.create(Sprite, {
         initialize: function (img, xCoord) {
             "use strict";
-            Sprite.call(this, 160, 240);
+            Sprite.call(this, img.width, img.height);
             
             this.image = img;
             this.x = xCoord;
@@ -102,9 +198,53 @@ var Indicator = {
             
             this.clock = new Timer.clock(this);
             this.onSwitch = new Switch.onOff(this);
+        },
+        
+        onenterframe: function () {
+            "use strict";
+        }
+    }),
+    
+    megapanel: Class.create(Sprite, {
+        initialize: function (img, xCoord) {
+            "use strict";
+            Sprite.call(this, img.width, img.height);
+            
+            this.image = img;
+            this.x = xCoord;
+            this.y = 240;
+            
+            this.timeLeft = Constants.seconds;
+            this.usable = true;
+            
+            // Four states
+            //  - 0: Off.
+            //  - 1: Frims decreasing, pazzles and gonks increasing.
+            //  - 2: Pazzles decreasing, frims and gonks increasing.
+            //  - 3: Gonks decreasing, frimz and pazzles increasing.
+            this.state = 0;
+            
+            this.clock = new Timer.clock(this);
+            this.onSwitch = new Switch.polystate(this);
+        },
+        
+        onenterframe: function () {
+            "use strict";
         }
     })
 };
+
+
+var Player = Class.create(Sprite, {
+    initialize: function (img) {
+        "use strict";
+        Sprite.call(this, img.width, img.height);
+        
+        this.image = img;
+        this.x = (Constants.stageWidth / 2) - (this.width / 2);
+        this.y = Constants.stageHeight - this.height;
+    }
+});
 
 
 var Scenes = {
@@ -112,19 +252,16 @@ var Scenes = {
         initialize: function (images) {
             "use strict";
             Scene.call(this);
-            var frims = new Sprite(180, 120);
-            var pazzles = new Sprite(180, 120);
-            var gonks = new Sprite(180, 120);
+            var frims = new Indicator.dial(images.frims, 25, 10, 90);
+            var pazzles = new Indicator.dial(images.pazzles, 230, 30, 85);
+            var gonks = new Indicator.dial(images.gonks, 435, 5, 50);
             var whatsit = new Indicator.panel(images.panel, 0);
             var thingymabob = new Indicator.panel(images.panel, 160);
             var doodad = new Indicator.panel(images.panel, 320);
-            var fixitall = new Indicator.panel(images.panel, 480);
+            var fixitall = new Indicator.megapanel(images.megapanel, 480);
+            var player = new Player(images.player);
             var children = [];
             var i;
-            
-            frims.image = images.frims;
-            frims.x = 25;
-            frims.y = 60;
             
             children.push(frims);
             children.push(pazzles);
@@ -133,6 +270,7 @@ var Scenes = {
             children.push(thingymabob);
             children.push(doodad);
             children.push(fixitall);
+            children.push(player);
             
             this.backgroundColor = "black";
             for (i = 0; i < children.length; i++) {
@@ -143,6 +281,12 @@ var Scenes = {
                 if (children[i].onSwitch) {
                     this.addChild(children[i].onSwitch);
                 }
+                if (children[i].safe) {
+                    this.addChild(children[i].safe);
+                }
+                if (children[i].danger) {
+                    this.addChild(children[i].danger);
+                }
             }
         }
     })
@@ -152,14 +296,34 @@ var Scenes = {
 $(document).ready(function () {
     "use strict";
     var game = new Core(Constants.stageWidth, Constants.stageHeight);
-    game.preload('img/panel.png',
-                 'img/dial.png');
+    game.preload('img/dial.png',
+                 'img/panel.png',
+                 'img/player.png',
+                 'img/safe.png',
+                 'img/warning.png');
     game.fps = 20;
     game.onload = function () {
         var images = {
+            frims: {
+                dial: game.assets['img/dial.png'],
+                warning: game.assets['img/warning.png'],
+                safe: game.assets['img/safe.png']
+            },
+            gonks: {
+                dial: game.assets['img/dial.png'],
+                warning: game.assets['img/warning.png'],
+                safe: game.assets['img/safe.png']
+            },
+            megapanel: game.assets['img/panel.png'],
             panel: game.assets['img/panel.png'],
-            frims: game.assets['img/dial.png']
+            pazzles: {
+                dial: game.assets['img/dial.png'],
+                warning: game.assets['img/warning.png'],
+                safe: game.assets['img/safe.png']
+            },
+            player: game.assets['img/player.png']
         };
+        
         var factoryScene = new Scenes.factory(images);
         game.pushScene(factoryScene);
     };
